d5e4709be3177ed033afb3410c1ccb3cdf535898
Sweetyamnesia <angela.r@live.fr>
2025-07-28 17:01:59 +0200

    change in comments

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 """
-This module contains function which takes float as argument
+This module provides a function that returns the floor
+of a given floating-point number.
 """
 
 
 import math
-"""
-importation of math module
-"""
 
 
 def floor(n: float) -> float:
     """
+    Returns the largest integer less than or equal
+    to the given floating-point number.
+
     Parameters:
-    n (float): the principal argument
+    n (float): A floating-point number.
 
     Returns:
-    the floor of the float
+    float: The floor of the input number.
     """
     return math.floor(n)
