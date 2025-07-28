0cfe6e54bf501ec76e22c58d838238927ffaa449
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:18:10 +0200

    add 4 types of variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module contains variables with integer, float,
+boolean and string
+"""
+
+
+a: int = 1  # variable int
+pi: float = 3.14  # variable float
+i_understand_annotations: bool = True  # variable boolean
+school: str = "Holberton"  # variable string
