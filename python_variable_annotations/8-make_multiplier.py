6afa467ff991a78ad4b4927719684d4960adbd17
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:38:55 +0200

    WIP: float object is not callable

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+This modules contains functions which takes float multiplier as argument
+"""
+
+
+def make_multiplier(multiplier: float) -> float:
+    """
+    Parameters:
+    multplier (float) : the principal argument
+
+    Returns:
+    The function which multiply the float
+    """
+    return float(multiplier)
