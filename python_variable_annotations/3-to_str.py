821332caecacba9f31983474015585cf50e63311
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:13:13 +0200

    add function to_str

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module contains function to_str which returns a string
+representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Parameters:
+    n (float) : the principal argument
+
+    Returns:
+    the string representation of the float
+    """
+    return str(n)
