3a68acea09743c934031ce327e11e983cf8210d6
Sweetyamnesia <angela.r@live.fr>
2025-07-28 17:31:15 +0200

    importation of List and Union, change comments

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-This module contains function which takes a list
-of integers and floats
+This module provides a function that computes
+the sum of a list containing both integers and floating-point numbers.
 """
 
+from typing import List, Union
 
-def sum_mixed_list(mxd_lst: int) -> float:
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
-    Parameters :
-    mxd_lst (int) : the mixed list
+    Parameters:
+    mxd_lst (List[Union[int, float]]): A list of numbers
+    (int and/or float) to sum.
 
     Returns:
-    the mixed list in float
+    float: The total sum as a floating-point number.
     """
-    return mxd_lst(float)
+    return sum(mxd_lst)
