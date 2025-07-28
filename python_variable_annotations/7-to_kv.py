da0bf673538ab7256ef8c0425c2d8bee1f07104a
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:33:56 +0200

    WIP : unsupported operand types

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+This module contains function which take string, int
+or float as arguments
+"""
+
+
+def to_kv(k: str, v: int | float):
+    """
+    Parameters:
+    k (str): a string (first argument)
+    v (int OR float): an int or float (second argument)
+
+    Returns:
+    tuple of the string k, and square of the float v.
+    """
+    return tuple
