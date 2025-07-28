25b48b6ec9970014c4069b23f5d8ebc5bc7e46b1
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:10:02 +0200

    add function floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+This module contains function which takes float as argument
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Parameters:
+    n (float): the principal argument
+
+    Returns:
+    the floor of the float
+    """
+    return n
