c445bd08033fd0194f866a5022ab99fbf1315ecf
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:07:01 +0200

    add function concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+This module contains function that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Parameters :
+    str1 (str): the first string
+    str2 (str): the second string
+
+    Returns:
+    str: a concatenate strings
+    """
+    result = str1 + str2
+    return result
