e3daf8f9c93023378a2ded5c48a86d0efb7a5e9f
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:43:49 +0200

    WIP : need to find appropriate type

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+This modules contains duck type with iterable objects
+"""
+
+
+def element_length(lst) -> list:
+    """
+    Parameters:
+    lst: the list
+
+    Returns:
+    the length of the list
+    """
+    return [(i, len(i)) for i in lst]
