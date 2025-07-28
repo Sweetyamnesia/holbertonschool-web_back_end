88d3c42fd636afef86ce9379e789abc8b930c131
Sweetyamnesia <angela.r@live.fr>
2025-07-28 16:27:07 +0200

    WIP : list object is not callable

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module contains function which takes a list
+of integers and floats
+"""
+
+
+def sum_mixed_list(mxd_lst: int) -> float:
+    """
+    Parameters :
+    mxd_lst (int) : the mixed list
+
+    Returns:
+    the mixed list in float
+    """
+    return mxd_lst(float)
