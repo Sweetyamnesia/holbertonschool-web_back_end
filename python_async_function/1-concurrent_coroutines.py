49941a33f693dda6de5aa344f9ea87573fda80ac
Sweetyamnesia <angela.r@live.fr>
2025-07-29 15:45:05 +0200

    add asynchrone and importation of the last function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import asyncio
+from typing import List
+import importlib.util
+
+module_name = "wait_random_module"
+module_path = "0-basic_async_syntax.py"
+
+spec = importlib.util.spec_from_file_location(module_name, module_path)
+mod = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(mod)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = asyncio.create_task(mod.wait_random(max_delay))
+    delays = await asyncio.gather(tasks)
+    return delays
