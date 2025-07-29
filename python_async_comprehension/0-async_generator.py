789c1b1b04a00b7f3812b7e9d545f752e4da17ae
Sweetyamnesia <angela.r@live.fr>
2025-07-29 17:17:26 +0200

    use async def + yield

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+This module defines an asynchronous generator that yields
+random integers between 0 and 10, one per second, 10 times.
+"""
+
+import asyncio
+from typing import AsyncGenerator
+import random
+
+
+async def async_generator() -> AsyncGenerator[int, None]:
+    """
+    Asynchronous generator that yields 10 random integers between 0 and 10.
+
+    Yields:
+    int: A random integer between 0 and 10 (inclusive), one per second.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
