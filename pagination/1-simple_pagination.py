197c9b14f9a81a8d2ca0670f2dceb2d5e2b4e3c3
Sweetyamnesia <angela.r@live.fr>
2025-08-11 14:07:38 +0200

    add previous function and modify get page. WIP : function get page

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -24,4 +24,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+        self.page = page
+        self.page_size = page_size
+        assert int (page) > 0 and int (page_size) > 0
+
+
+    def index_range(page, page_size):
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
