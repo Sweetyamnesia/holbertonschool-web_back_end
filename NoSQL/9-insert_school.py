62a8c6c596fc6e6756a747d4af9d848bfd0d9127
Sweetyamnesia <angela.r@live.fr>
2025-08-13 09:39:01 +0200

    WIP: start the function

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+This module contains a function that inserts
+a new document in a collection.
+"""
+
+import pymongo
+
+
+# establishing connection to the database
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    
+    """
+    documents = list(mongo_collection.find({}))
+
