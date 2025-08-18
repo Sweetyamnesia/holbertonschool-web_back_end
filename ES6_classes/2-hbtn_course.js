53ae19ff25f50b715bfe9e037dbb87ed3f3c1f83
Sweetyamnesia <angela.r@live.fr>
2025-08-18 14:49:04 +0200

    add getter and setter for each attribute

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,4 +4,37 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string.');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number' || newLength < 0) {
+      throw new Error('Length must be a non-negative number.');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new Error('Students must be an array.');
+    }
+    this._students = newStudents;
+  }
 }
