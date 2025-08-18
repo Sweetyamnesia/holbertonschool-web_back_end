d2c063fff086cc845be18e52782d0aaf133fd1e7
Sweetyamnesia <angela.r@live.fr>
2025-08-18 10:40:16 +0200

    try rest parameter with args

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,3 +1,7 @@
-export default function returnHowManyArguments() {
-
+export default function returnHowManyArguments(...theArgs) {
+	let total = 0;
+  for (const arg of theArgs) {
+    total += arg;
+  }
+  return total;
 }
