3609a1f7edfb5e216775fcb7d64f468714865460
Sweetyamnesia <angela.r@live.fr>
2025-08-18 15:01:07 +0200

    add getter and setter, but wip for displayfullcurrency

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -3,4 +3,30 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new Error('Code must be a string.');
+    }
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string.');
+    }
+    this._name = newName;
+  }
+
+  get displayFullCurrency() {
+    return `${this._name} ${this._code}`;
+  }
 }
