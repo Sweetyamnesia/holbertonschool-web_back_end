5973ea029cb70911a2eed5397383b1ffe4dfad08
Sweetyamnesia <angela.r@live.fr>
2025-08-19 14:52:19 +0200

    delete importation and add method to string

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,8 +1,10 @@
-import _default from "jest";
-
 export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
+  }
+
+  toString() {
+	return [this._code];
   }
 }
