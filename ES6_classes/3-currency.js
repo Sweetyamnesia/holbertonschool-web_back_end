2e411b5fd0a38fd0bbe7f0fdee8472c760c110b9
Sweetyamnesia <angela.r@live.fr>
2025-08-19 09:01:23 +0200

    fix: update displayFullCurrency to show name (code) format and change error for TypeError

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -10,7 +10,7 @@ export default class Currency {
 
   set code(newCode) {
     if (typeof newCode !== 'string') {
-      throw new Error('Code must be a string.');
+      throw new TypeError('Code must be a string.');
     }
     this._code = newCode;
   }
@@ -21,12 +21,12 @@ export default class Currency {
 
   set name(newName) {
     if (typeof newName !== 'string') {
-      throw new Error('Name must be a string.');
+      throw new TypeError('Name must be a string.');
     }
     this._name = newName;
   }
 
-  get displayFullCurrency() {
-    return `${this._name} ${this._code}`;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
