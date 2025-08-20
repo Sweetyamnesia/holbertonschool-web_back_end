53b9d86a4e9a50ff3e393cba1a84fbc440412f0f
Sweetyamnesia <angela.r@live.fr>
2025-08-20 12:20:09 +0200

    delete tab character

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -3,8 +3,8 @@ export default class HolbertonCourse {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
     if (!Array.isArray(students)) throw new TypeError('Students must be an array');
-	
-	this._name = name;
+
+    this._name = name;
     this._length = length;
     this._students = students;
   }
