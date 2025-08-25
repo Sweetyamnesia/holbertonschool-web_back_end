c7062c7abf3d219ec046b60c436bdf5a9c0938a4
Sweetyamnesia <angela.r@live.fr>
2025-08-25 16:19:42 +0200

    fix handleprofilesignup : resolve and log promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,11 +4,13 @@ import createUser from "./utils.js";
 export default function handleProfileSignup() {
 	const promise1 = uploadPhoto();
 	const promise2 = createUser();
-	Promise.all([promise1, promise2]).then(data => {
-		return ({body, firstName, lastName});
+	return Promise.all([promise1, promise2]).then(data => {
+		const [photo, user] = data;
+		console.log(photo.body, user.firstName, user.lastName);
+		return (data);
 	})
 	.catch(error => {
 		console.log("Signup system offline");
-		return new Error();
+		return Promise.reject(new Error());
 	});
 }
