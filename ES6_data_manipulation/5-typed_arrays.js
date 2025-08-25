b5483fcab18b35dd9a2a849b449017689102b3cc
Sweetyamnesia <angela.r@live.fr>
2025-08-25 11:58:45 +0200

    add condiition and throw error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
-  int8[position] = value;
-  return int8;
+  const int8Array = new Int8Array(buffer);
+  if (position < 0 || position >= length) {
+    throw new Error("Position outside range");
+  }
+  int8Array[position] = value;
+  return buffer;
 }
