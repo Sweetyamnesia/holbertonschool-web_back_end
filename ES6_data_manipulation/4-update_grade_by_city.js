766b4ba18d0784bd8c7d7b1b0fcfa19e8460a5b0
Sweetyamnesia <angela.r@live.fr>
2025-08-25 17:14:52 +0200

    fix: map each student to a new object with updated grade or 'N/A'

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,21 @@
-export default function updateStudentGradeByCity(students, city) {
-  const filtered = students.filter((students) => students.location === city);
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const filtered = students.filter(student => student.location === city);
 
-  return filtered.map((student) => student.id)
+  return filtered.map(student => {
+    const gradeObj = newGrades.find(grade => grade.studentId === student.id);
 
+    let grade;
+    if (gradeObj) {
+      grade = gradeObj.grade;
+    } else {
+      grade = "N/A";
+    }
+
+    return {
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: grade
+    };
+  });
 }
