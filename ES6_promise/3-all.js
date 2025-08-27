6e3b74d8adb03be5ec437bfd45ca1ae1015d0414
Sweetyamnesia <angela.r@live.fr>
2025-08-27 06:01:10 +0200

    fix(handleProfileSignup): improve promise handling and error management

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,15 +1,12 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  const promise1 = uploadPhoto();
-  const promise2 = createUser();
-  return Promise.all([promise1, promise2])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
-      return [photo, user];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
-      return Promise.reject(new Error());
     });
 }
