a56e228277c3e8c219f16c43cd0ccb11a4c48145
Sweetyamnesia <angela.r@live.fr>
2025-08-27 06:22:35 +0200

    fix: delete brackets and add parenthesis

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,6 +4,6 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error("Position outside range");
   }
-  view.setInt8[position] = value;
+  view.setInt8(position, value);
   return view;
 }
