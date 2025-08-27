33d4e74020f9a776d0190609e391d097f06ebe72
Sweetyamnesia <angela.r@live.fr>
2025-08-27 06:12:21 +0200

    fix(uploadPhoto): ensure promise is properly rejected on error

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,7 +1,3 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => {
-	const data = fileName;
-	reject(new Error(`${fileName} cannot be processed`));
-
-  });
-}
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
+  }
