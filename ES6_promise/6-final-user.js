4b471ceb3a2cf95bed997c97f5488b57ba940cb0
Sweetyamnesia <angela.r@live.fr>
2025-08-27 06:43:31 +0200

    fix(handleProfileSignup): correct promise handling and console output

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
-import signUpuser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import { signUpuser } from "./4-user-promise.js";
+import { uploadPhoto } from "./5-photo-reject.js";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-	const promise1 = signUpuser();
-		const promise2 = uploadPhoto();
-		return Promise.all([promise1, promise2]).then(data => {
-			const [photo, user] = data;
-			console.log(photo.body, user.firstName, user.lastName);
-			return (data);
-		})
-		.catch(error => {
+export default function handleProfileSignup() {
+	return Promise
+	  .all(([signUpuser(), uploadPhoto()]))
+	  .then(([user, photo]) => {
+		console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+	   })
+	    .catch(() => {
 			console.log("Signup system offline");
-			return Promise.reject(new Error());
 		});
 }
