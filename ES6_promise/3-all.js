fb05c509749c380d776fb95e7ddab92e5bfa5903
Sweetyamnesia <angela.r@live.fr>
2025-08-27 05:56:32 +0200

    fix : lint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,15 +1,15 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	const promise1 = uploadPhoto();
-	const promise2 = createUser();
-	return Promise.all([promise1, promise2])
-	.then(([photo, user]) => {
-		console.log(photo.body, user.firstName, user.lastName);
-		return [photo, user];
-	})
-	.catch(() => {
-		console.log("Signup system offline");
-		return Promise.reject(new Error());
-	});
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+  return Promise.all([promise1, promise2])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
+      return [photo, user];
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+      return Promise.reject(new Error());
+    });
 }
