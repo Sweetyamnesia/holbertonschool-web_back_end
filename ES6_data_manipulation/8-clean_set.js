55c99205924b6c98a7069c3eb75be28dc210bad7
Sweetyamnesia <angela.r@live.fr>
2025-08-28 12:16:40 +0200

    change substring for slice

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   return Array.from(set)
     .filter(value => value.substring(0, startString.length) === startString)
-    .map(value => value.substring(startString.length))
+    .map(value => value.slice(startString.length))
     .join('-');
 }
