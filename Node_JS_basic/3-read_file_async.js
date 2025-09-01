1bff920f114ba87a34d7629e4e34304b105c57b6
Sweetyamnesia <angela.r@live.fr>
2025-09-01 13:59:21 +0200

    fix: ESLint

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises;
 
 function countStudents(path) {
-	fs.readFile(path, 'utf8')
-	.then(data => {
-		console.log(`Number of students in FIELD: 6. List: ${LIST_OF_FIRSTNAMES}`, data);
-	})
-	.catch(err => console.error('Cannot load the database'));
+  fs.readFile(path, 'utf8')
+    .then((data) => {
+      console.log(`Number of students in FIELD: 6. List: ${LIST_OF_FIRSTNAMES}`, data);
+    })
+    .catch((err) => console.error('Cannot load the database'));
 
-	console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 }
 
 module.exports = countStudents;
