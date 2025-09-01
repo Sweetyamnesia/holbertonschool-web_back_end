05a075939bcee794f0d0b288df9b369a2d867a0a
Sweetyamnesia <angela.r@live.fr>
2025-09-01 12:19:17 +0200

    problem with the file 3

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,13 +1,27 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-	if (req.url === '/') {
-		res.writeHead(200, { 'Content-Type': 'text/plain' });
-  		res.end('Hello Holberton School!');
-	}
-	else if (req.url === '/students') {
-		res.write('This is the list of our students\n');
-	}
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+
+    countStudents(database)
+      .then(output => {
+        res.end(output);
+      })
+      .catch(() => {
+        res.end('Cannot load the database\n');
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
 });
 
 app.listen(1245, () => {
