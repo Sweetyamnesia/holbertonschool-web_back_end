1d1663da0c6f59bcad2af77539fe318fa7c3104f
Sweetyamnesia <angela.r@live.fr>
2025-09-01 12:01:54 +0200

    add modifications

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,12 +1,11 @@
+const fs = require('fs');
+
 function countStudents(path) {
-	const fs = require('fs');
-	const contenu = fs.readFileSync(path, 'utf8');
 	if (!fs.existsSync(path)) {
-		throw Error('Cannot load the database');
+		throw new Error('Cannot load the database');
 	}
-	else {
-		console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
-	}
+	const contenu = fs.readFileSync(path, 'utf8');
+	console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 }
 
 module.exports = countStudents;
