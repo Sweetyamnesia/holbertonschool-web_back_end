9519447ee45472f40f484d52ac811c03c15755f3
Sweetyamnesia <angela.r@live.fr>
2025-09-01 12:24:34 +0200

    add function countStudents, the database and module

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
 
+const countStudents = require('./3-read_file_async');
+const database = process.argv[2];
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-	res.send(`This is the list of our students`);
+	res.write(`This is the list of our students\n`);
 });
+
+countStudents(database)
+    .then(output => {
+      res.end(output);
+    })
+    .catch(() => {
+      res.end('Cannot load the database\n');
+    });
 
 app.listen(1245, () => {
   console.log('Serveur Express démarré sur http://localhost:1245');
 });
+
+module.exports = app;
