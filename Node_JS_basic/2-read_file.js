883fff5ac36ee16095ae08c8134c833ea19c1524
Sweetyamnesia <angela.r@live.fr>
2025-09-01 13:56:30 +0200

    fix: ESLint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 function countStudents(path) {
-	if (!fs.existsSync(path)) {
-		throw new Error('Cannot load the database');
-	}
-	const contenu = fs.readFileSync(path, 'utf8');
-	console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  const contenu = fs.readFileSync(path, 'utf8');
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 }
 
 module.exports = countStudents;
