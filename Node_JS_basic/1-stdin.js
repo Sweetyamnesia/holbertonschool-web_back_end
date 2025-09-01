8b2b489553dd37f1d1c6f76b79bdd78d63aa1ed3
Sweetyamnesia <angela.r@live.fr>
2025-09-01 13:53:57 +0200

    fix: ESLint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name? \n', input => {
-	console.log(`Your name is: ${input}`);
-	rl.close();
+rl.question('Welcome to Holberton School, what is your name? \n', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
 });
 
 rl.on('close', () => {
