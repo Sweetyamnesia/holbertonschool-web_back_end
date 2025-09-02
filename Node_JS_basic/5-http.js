fcb10a20b0e1735f6d197e1ffe79bc3cd1dc0b16
Sweetyamnesia <angela.r@live.fr>
2025-09-02 11:12:03 +0200

    feat(server): update HTTP server routes and integrate async students list

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,28 +4,26 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
     countStudents(database)
-      .then((output) => {
-        res.end(output);
+      .then((studentsList) => {
+        res.end(studentsList);
       })
       .catch(() => {
-        res.end('Cannot load the database\n');
+        res.end('Cannot load the database');
       });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
+    res.end('Not Found');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server running at http://localhost:1245');
-});
+app.listen(1245);
 
 module.exports = app;
