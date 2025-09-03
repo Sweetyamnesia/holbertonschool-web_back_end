c64bb4f78a67f989bf7672137f902015795645ac
Sweetyamnesia <angela.r@live.fr>
2025-09-03 12:06:58 +0200

    try an other version

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-
-  if (process.stdin.isTTY) {
-    process.stdin.pause();
-  }
+  const name = data;
+  process.stdout.write(`Your name is: ${name}`);
+  process.exit(0);
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
