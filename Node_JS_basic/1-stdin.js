8ec417707d7dab44a2a84b8f4deacb1b880082c8
Sweetyamnesia <angela.r@live.fr>
2025-09-03 11:21:13 +0200

    modif

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,11 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
-if(!process.stdin.isTTY) {
-	process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
+if (!process.stdin.isTTY) {
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
 }
