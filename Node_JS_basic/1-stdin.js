7c90fe50104c508c0d10c580fe6be2c81474e2d1
Sweetyamnesia <angela.r@live.fr>
2025-09-03 11:26:12 +0200

    fix: add condition to stop the loop

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+
+  if (process.stdin.isTTY) {
+    process.stdin.pause();
+  }
 });
 
-if (!process.stdin.isTTY) {
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
-  });
-}
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
