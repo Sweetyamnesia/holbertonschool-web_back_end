e11d4ec15870c5c52c99841cea10cc1a058a5046
Sweetyamnesia <angela.r@live.fr>
2025-09-03 13:49:53 +0200

    add modifications

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,22 @@
 const express = require('express');
-
-const app = express();
-
 const countStudents = require('./3-read_file_async');
 
+const app = express();
 const database = process.argv[2];
 
 app.get('/', (req, res) => {
-	res.type('text/plain');
-    res.send('Hello Holberton School!');
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-	res.type('text/plain');
-    res.send('This is the list of our students\n');
+  res.type('text/plain');
 
   try {
     const studentsList = await countStudents(database);
-    res.send(`This is the list of our students\n${studentsList || ''}`);
+    res.send(`This is the list of our students\n${studentsList}`);
   } catch {
-    res.end('Cannot load the database');
+    res.send('Cannot load the database');
   }
 });
 
